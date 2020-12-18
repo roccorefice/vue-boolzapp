db0018e76a7f7b36740117579961098eae1e71be
Rocco Orefice <rocco.orefice92@gmail.com>
2020-12-18 17:10:55 +0100

    style css e inizio della stampa dei contatti con vueJs

---
+++ Millestone_1/assets/js/main.js
@@ -0,0 +1,42 @@
+let app = new Vue ({
+    el: "#app",
+    data : {
+        contatti: [
+            {
+                name: "Ron 🦸",
+                avatar: './assets/img/ron.jpg',
+                // visible: true,
+                // messagges:[
+                //     {
+                //         date: '10/01/2020 15:30',
+                //         text: 'Harry!!!',
+                //         status: 'received'
+                //     },
+                //     {
+                //         date: '10/01/2020 15:31',
+                //         text: 'Harryyyyy ci sei???',
+                //         status: 'received'
+                //     },
+                //     {
+                //         date: '10/01/2020 15:31',
+                //         text: 'Ho rotto di nuovo la bacchetta!!! 😭😭😭',
+                //         status: 'received'
+                //     },
+                //     {
+                //         date: '10/01/2020 16:12',
+                //         text: 'No NON CI CREDO! Fai sempre questo Ron! 🙆‍♂️',
+                //         status: 'sent'
+                //     }
+                //]
+            }
+        ]
+       
+        
+    },
+    // methods: {
+ 
+    // },
+    // mounted: function(){
+        
+    // }
+});
